c3a91042331277f3026d33a6fa0b79e47cc59b0d
Leon <leon.ser.e83@gmail.com>
2018-07-05 01:46:30 +0300

    начало доработки Добавление работі в браузере ХРОМ

--- src/main/java/ua/home/NovaPoshta/MainClassNP.java
+++ src/main/java/ua/home/NovaPoshta/MainClassNP.java
@@ -1,21 +1,26 @@
 package ua.home.NovaPoshta;
-
-import com.google.common.collect.Lists;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import sun.plugin2.util.BrowserType;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainClassNP {
     static WebDriver wd;
 
     public static void main(String[] args) {
+        String broeser = org.openqa.selenium.remote.BrowserType.CHROME;
 
-        wd = new FirefoxDriver();
+        if (broeser == org.openqa.selenium.remote.BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if ( broeser == org.openqa.selenium.remote.BrowserType.CHROME) {
+            System.setProperty("webdriver.chrome.driver", "W:\\TESTER\\WebDriverSel\\Demo_sel\\NovaPoshta_AT\\drivers\\chromedriver.exe");
+            wd = new ChromeDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        wd.manage().window().maximize();
+//        wd.manage().window().maximize();
         wd.get("https://novaposhta.ua");
         MainPageNP mainPageNP = new MainPageNP(wd);
 
