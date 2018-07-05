9d600b942249500bd144fd8e5336a185644e26fe
Leon <leon.ser.e83@gmail.com>
2018-07-06 01:05:02 +0300

    Описание полей Формы на deliveryPage

--- src/main/java/ua/home/NovaPoshta/DeliveryPageNP.java
+++ src/main/java/ua/home/NovaPoshta/DeliveryPageNP.java
@@ -14,6 +14,26 @@ public class DeliveryPageNP {
     // Описание Елементов на странице
     private By headerDeliveryPage = By.xpath(".//*[@id='wrapper']/h1");
     private By mainPagelink = By.xpath(".//a[@title='Головна']");
+    // елементы формы
+    private By deliverysenderCityField = By.xpath(".//*[@id='DeliveryForm_senderCity']");
+    private By deliveryrecipientCityField = By.xpath(".//*[@id='DeliveryForm_recipientCity']");
+    private By deliveryTechnologyField = By.xpath(".//input[@id='DeliveryForm_deliveryTechnology_id']");
+    private By deliveryTechnologyList = By.xpath(".//div[@class='select deliveryTechnology del_type']/div[@class='dropdown']/ul/li");
+    private By deliveryWeightField = By.xpath(".//*[@id='DeliveryForm_weight']");
+    private By deliveryHeightField = By.xpath(".//*[@id='DeliveryForm_height']");
+    private By deliveryWidthField = By.xpath(".//*[@id='DeliveryForm_width']");
+    private By deliveryDepthField = By.xpath(".//*[@id='DeliveryForm_depth']");
+    private By deliveryPlacesCountField = By.xpath(".//input[@id='DeliveryForm_places_count']");
+    private By deliveryPublicPriceField = By.xpath(".//input[@id='DeliveryForm_publicPrice']");
+    private By deliveryPackingServiceCheckBox = By.xpath(".//*[@id='DeliveryForm_packing_service']");
+    private By deliverybackDeliveryCheckBox = By.xpath(".//*[@id='DeliveryForm_backDelivery']");
+    private By deliveryCalcButton = By.xpath(".//input[@value='Рассчитать стоимость']");
+    private By deliveryClearAllFieldButton = By.xpath(".//input[@value='Очистить']");
+    private By deliveryInvalidElemrtsList = By.xpath(".//label[contains(@class,'invalid')]");
+    private By deliveryValidElementsList = By.xpath(".//label[contains(@class,' valid')]");
+
+
+
 
     // Описание методов MainPageNP
     public String getHeaderDeliveryPage() {
